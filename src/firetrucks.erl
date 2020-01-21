790be4b96401dbc6ca1f0bce41cc2ff708a22ee9
Kacper Tonia <kacpert314@gmail.com>
2020-01-21 21:49:21 +0100

    Convert firetruck record to a map

--- src/firetrucks.erl
+++ src/firetrucks.erl
@@ -1,10 +1,13 @@
 -module(firetrucks).
--export([make/1]).
+-export([make/1, as_map/1]).
 
 -record(firetruck, {
     id,
     status = ready
 }).
+
+as_map(#firetruck{id=Id, status=Status}) ->
+    #{<<"id">>=>Id, <<"status">>=>Status}.
 
 
 make(Identifier) ->
