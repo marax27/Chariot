301b36bf7e1eb94e9bdfe2019f3b25c2e4683086
Kacper Tonia <kacpert314@gmail.com>
2020-01-22 11:52:36 +0100

    Display vehicles on webpage

--- website/scripts.js
+++ website/scripts.js
@@ -19,10 +19,29 @@ function GetFireTrucks() {
         if(data) {
             const firetrucks = JSON.parse(data);
             LogMessage(firetrucks);
+            UpdateVehicleSection(firetrucks);
         }
     });
 
     setTimeout(GetFireTrucks, 1000);
+}
+
+function UpdateVehicleSection(vehicles) {
+    var box = document.getElementById("vehicle-box");
+    RemoveAllChildren(box);
+
+    Array.prototype.forEach.call(vehicles, vehicle => {
+        const elem = BuildBox(vehicle);
+        box.appendChild(elem);
+    });
+}
+
+function BuildBox(vehicle) {
+    var elem = document.createElement('div');
+    elem.textContent = vehicle.id;
+    elem.setAttribute('class', 'box ' + vehicle.status);
+    elem.clientHeight = elem.clientWidth;
+    return elem;
 }
 
 function LogMessage(obj) {
@@ -38,6 +57,12 @@ function CurrentTime() {
     const s = String(now.getSeconds()).padStart(2, '0');
     const ms = String(now.getMilliseconds()).padStart(3, '0');
     return `${h}:${m}:${s}.${ms}`;
+}
+
+function RemoveAllChildren(node) {
+    while(node.firstChild) {
+        node.removeChild(node.firstChild);
+    }
 }
 
 GetFireTrucks();
